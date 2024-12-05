83904598c33ce89ad91d1848bab8c80285eb58e7
salimkhandev <salimt450s@gmail.com>
2024-12-05 17:31:45 +0500

    improved

---
+++ index.js
@@ -0,0 +1,33 @@
+const hamburgerButton = document.querySelector('.hamburger');
+        const navMenu = document.querySelector('.navbar');
+
+   
+   hamburgerButton.addEventListener('click', () => {
+        navMenu.classList.toggle('open');
+    });
+
+    const images = document.querySelectorAll('.carousel-images img');
+        const prevButton = document.querySelector('.control.prev');
+        const nextButton = document.querySelector('.control.next');
+
+        let currentIndex = 0;
+
+        // Function to show the current image
+        function showImage(index) {
+            images.forEach((img, i) => {
+                img.classList.toggle('active', i === index);
+            });
+        }
+
+   
+        prevButton.addEventListener('click', () => {
+            currentIndex = (currentIndex - 1 + images.length) % images.length;
+            showImage(currentIndex);
+        });
+
+        nextButton.addEventListener('click', () => {
+            currentIndex = (currentIndex + 1) % images.length;
+            showImage(currentIndex);
+        });
+
+        showImage(currentIndex);
