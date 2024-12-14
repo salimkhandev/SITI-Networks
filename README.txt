4f61c96155ab6f71cdafdda05422e2a014118fcf
salimkhandev <salimt450s@gmail.com>
2024-12-15 02:54:30 +0500

    improved

--- README.txt
+++ README.txt
@@ -4,5 +4,6 @@ Name: Salim Khan
 Section: A
 Class No: 221232
 Semester: 5th
+Subject:Internet Programming
 Department: Software Engineering
 Submitted To: Dr. Tauseef Ur Rahman
--- index.js
+++ index.js
@@ -1,32 +1,28 @@
 const hamburgerButton = document.querySelector('.hamburger');
-        const navMenu = document.querySelector('.navbar');
-
-   
-   hamburgerButton.addEventListener('click', () => {
-        navMenu.classList.toggle('open');
+const navMenu = document.querySelector('.navbar');
+const fa_xmark = document.querySelector('.fa-xmark');
+const fa_bars = document.querySelector('.fa-bars');
+hamburgerButton.addEventListener('click', () => {
+    navMenu.classList.toggle('open');
+    fa_xmark.classList.toggle('toggleXmarks');
+    fa_bars.classList.toggle('fa-barsToggle');
+  
+});
+const images = document.querySelectorAll('.carousel-images img');
+const prevButton = document.querySelector('.control.prev');
+const nextButton = document.querySelector('.control.next');
+let currentIndex = 0;
+function showImage(index) {
+    images.forEach((img, i) => {
+        img.classList.toggle('active', i === index);
     });
-
-    const images = document.querySelectorAll('.carousel-images img');
-        const prevButton = document.querySelector('.control.prev');
-        const nextButton = document.querySelector('.control.next');
-
-        let currentIndex = 0;
-
-        // Function to show the current image
-        function showImage(index) {
-            images.forEach((img, i) => {
-                img.classList.toggle('active', i === index);
-            });
-        }
-
-        prevButton.addEventListener('click', () => {
-            currentIndex = (currentIndex - 1 + images.length) % images.length;
-            showImage(currentIndex);
-        });
-
-        nextButton.addEventListener('click', () => {
-            currentIndex = (currentIndex + 1) % images.length;
-            showImage(currentIndex);
-        });
-
-        showImage(currentIndex);
+}
+prevButton.addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    showImage(currentIndex);
+});
+nextButton.addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    showImage(currentIndex);
+});
+showImage(currentIndex);
